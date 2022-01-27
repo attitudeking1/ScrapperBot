d18a1bc24ee58695281404c38251c198f09fce9d
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-01-27 13:40:03 +0530

    Ummm

--- main.py
+++ main.py
@@ -45,20 +45,6 @@ with open("data.csv", encoding='UTF-8') as f:
             PREMIUM.append(int(row[1]))
 
 # ------------------------------- Subscribe --------------------------------- #
-async def Subscribe(lel, message):
-   update_channel = UPDATES_CHANNEL
-   if update_channel:
-      try:
-         user = await app.get_chat_member(update_channel, message.chat.id)
-         if user.status == "kicked":
-            await app.send_message(chat_id=message.chat.id,text="Sorry Sir, You are Banned. Contact My [Support Group](https://t.me/InducedBotsSupport).", parse_mode="markdown", disable_web_page_preview=True)
-            return 1
-      except UserNotParticipant:
-         await app.send_message(chat_id=message.chat.id, text="**Please Join My Updates Channel To Use Me!\n and click on to Check /start**", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🤖 Join Updates Channel 🤖", url=f"https://t.me/{update_channel}")]]), parse_mode="markdown")
-         return 1
-      except Exception:
-         await app.send_message(chat_id=message.chat.id, text="**Something Went Wrong. Contact My [Support Group](https://t.me/InducedBotsSupport).**", parse_mode="markdown", disable_web_page_preview=True)
-         return 1
 
 
 
