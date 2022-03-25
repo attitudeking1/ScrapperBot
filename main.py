d63f682347a76c2d42abed0cbfaeff65fe7a659e
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-03-25 10:44:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ API_HASH = "4e984ea35f854762dcde906dce426c2d"
 BOT_TOKEN = "5245262570:AAFN4y8NkomLxmfzhZoiX-uuKjKZBr_0XiU"
 UPDATES_CHANNEL = "Superior_Bots"
 OWNER= [936481432,1669178360]
-PREMIUM= [Superior_bots, Superior_Support]
+PREMIUM= ["Superior_bots", "Superior_Support"]
 app = pyrogram.Client("app", api_id=APP_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 with open("data.csv", encoding='UTF-8') as f:
